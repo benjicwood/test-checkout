41bb8857cf2644848cc49fb65735fd590a886ced
benjicwood <bencharlwood@gmail.com>
2017-10-27 10:31:16 +0100

    make App test work with changes made;

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,8 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+
+import { createStore } from 'redux';
+import allReducers from '../../reducers/index.js';
+
+const store = createStore(allReducers);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}><App /></ Provider>, div);
 });
